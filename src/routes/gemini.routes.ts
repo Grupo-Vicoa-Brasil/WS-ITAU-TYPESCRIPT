c79df695e7c71665dffd3f580dc1d373f3f48884
Antonio Bernardino da Silva <tonybsilvadev@gmail.com>
2021-02-16 11:22:38 -0300

    port correction

--- src/routes/gemini.routes.ts
+++ src/routes/gemini.routes.ts
@@ -9,15 +9,15 @@ app.use(express.json())
 
 export default function Gemini (req, res) {
   // Opens the url in the default browser
-  open('45.179.89.8:33333/itau')
-  open('45.179.89.8:33333/itau2')
-  open('45.179.89.8:33333/itau4')
-  open('45.179.89.8:33333/itau5')
-  open('45.179.89.8:33333/itau6')
-  open('45.179.89.8:33333/itau7')
-  open('45.179.89.8:33333/itau8')
-  open('45.179.89.8:33333/itau9')
-  open('45.179.89.8:33333/itau10')
+  open('45.179.89.8:3333/itau')
+  open('45.179.89.8:3333/itau2')
+  open('45.179.89.8:3333/itau4')
+  open('45.179.89.8:3333/itau5')
+  open('45.179.89.8:3333/itau6')
+  open('45.179.89.8:3333/itau7')
+  open('45.179.89.8:3333/itau8')
+  open('45.179.89.8:3333/itau9')
+  open('45.179.89.8:3333/itau10')
 
   console.log(new AppSuccess(' Todas as aplicações foram iniciadas com sucesso!'))
 }
